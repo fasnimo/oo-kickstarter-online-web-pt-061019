748a8864499f41ae1c70322913084c45996ed89a
Rene Cocom <fasnimo@gmail.com>
2019-07-31 16:51:25 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(project, backer)
+  def back_project(project, :backer)
     @backed_projects << project
     @backer_project <<
   end
