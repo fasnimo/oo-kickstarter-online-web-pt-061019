516dade6a0c9788c75f0e79be8b6e3b7b2b40015
Rene Cocom <fasnimo@gmail.com>
2019-07-31 16:44:58 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,7 @@ class Backer
   attr_accessor :backed_projects
   attr_reader :name
 
-  def initialize
+  def initialize(name)
     @name = name
     # @backed_projects = backed_projects
     @backed_projects = []
